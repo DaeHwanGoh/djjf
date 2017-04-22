61bdd39e7656b095bc24d7671b36490815b15252
Windows7_64 <Windows7_64@Win7_64-PC>
2017-04-23 01:52:58 +0900

    Gson Test클래스를 참고하여 매니저클래스 테스트클래스를 작성하였습니다. 매니저클래스가 갖고있는 필드에 값이 잘 할당되었는 지와 노드간의 연결이 잘 되었는지를 테스트하였습니다.

--- project2/src/daum/cafe/service/svccafeinfo/manager/test/CafeCategoryMgrTest.java
+++ project2/src/daum/cafe/service/svccafeinfo/manager/test/CafeCategoryMgrTest.java
@@ -37,7 +37,26 @@ public class CafeCategoryMgrTest {
 		assertThat(allCate.length, is(categorys.length));
 		//주어진 Json데이터와 DB에서 불러오는 데이터가 같다고 가정하고 테스트
 		
-		
+		List<String> mgrChildIdList=new ArrayList<>();
+		for(int i=0; i<categorys.length; i++){
+		//json데이터를 봤을 때 부모,서브,서브... 부모,서브,서브... 순으로 정렬돼있어서 Json데이터로 for문을 돌렸습니다.
+			CafeCategoryNode jsonNode=new CafeCategoryNode(categorys[i]);
+			if(jsonNode.hasParent()){
+				assertThat(mgr.getNode(categorys[i].cateid).getParent().getId(), 
+																is(categorys[i].parcateid));
+				//만약 부모카테고리가 있다면 부모노드연결이 성공적으로 연결되었는지 비교
+
+				assertTrue(mgrChildIdList.contains(categorys[i].cateid));
+				//부모->서브 연결이 빠짐없이 잘 되어있는지 확인
+			}else{
+				mgrChildIdList=new ArrayList<>();
+				//Json데이터 순서를 참고하여 새로운 부모가 나오면 리스트를 초기화하고 리스트에 자식노드의 ID를 담았습니다.
+				for(CafeCategoryNode tmp : mgr.getNode(categorys[i].cateid).getChildrens()){
+					mgrChildIdList.add(tmp.getId());
+				}
+				assertThat(mgr.get(categorys[i].cateid).cateid, is(categorys[i].cateid));
+			}
+		}
 	}
 
 	private InputStreamReader getJson() throws UnsupportedEncodingException {
